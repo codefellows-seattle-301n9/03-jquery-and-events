7c5cb966a9f05886b95b1691def6bbb1b5d5db83
ErmineNotyours <arthurallen@centurylink.net>
2017-12-07 21:03:51 -0800

    Added code, with errors

--- starter-code-arthur-mitch/scripts/articleView.js
+++ starter-code-arthur-mitch/scripts/articleView.js
@@ -39,13 +39,13 @@ articleView.handleAuthorFilter = function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
 
     if ($(this).val()) {
-      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      // TODO DONE?: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
-      $('article').hide;
-      //$(`article[data-author"${$(this.val()}"]"`)
+      $('article').hide();
+      $(`article[data-author="${$(this).val()}"]`).fadein();
 
     } else {
-      // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+      // DONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $('article').fadein();
       $('article.template').hide();
     }
@@ -54,17 +54,41 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+  // DONE: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $(`article[data-category="${$(this).val()}"]`).fadein();
 
+    } else {
+      $('article').fadein();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
 };
 
+
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+
+  // DONE?: Add an event handler to .main-nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
+  $('#main-nav').on('change', function() {
+    if ($(this).val()) {
+      $('tab').hide();
+      $(`tab[data-category="${$(this).val()}"]`).fadein();
+
+    } else {
+      $('tab').fadein();
+      $('tab.template').hide();
+    }
+    $('#main-nav').val('');
+  });
+
 
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
   $('.main-nav .tab:first').click();
@@ -76,6 +100,22 @@ articleView.setTeasers = function() {
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+
+  // Done, with errors: "DOMException: Failed to execute 'querySelectorAll' on 'Element': '*,:x' is not a valid selector." jQuery error
+  $('article').on('click', 'a.read-on', function(e) {
+    e.preventDefault();
+    if ($(this).text() === 'Read on &rarr;') {
+      console.log($(this).text());
+      $(this).parent().find('*').fadeIn();
+      $(this).html('Show Less &larr;');
+    } else {
+      $('body').animate({
+        scrollTop: ($(this).parent().offset().top)
+      }, 200);
+      $(this).html('Read on &rarr;');
+      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+    }
+  });
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
