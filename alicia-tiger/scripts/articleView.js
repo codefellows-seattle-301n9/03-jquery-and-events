34d41ab0d85aba446edaee5cf653938242acbd08
Alicia Lycan <alicialycan@gmail.com>
2017-12-07 19:10:30 -0800

    Add Hide/Show Methods

--- alicia-tiger/scripts/articleView.js
+++ alicia-tiger/scripts/articleView.js
@@ -4,7 +4,7 @@
 let articleView = {};
 
 articleView.populateFilters = function() {
-  $('article').each(function() {
+  $('article').each(function() { //sends you back a selection of everywhere your selector is
     // REVIEW: We can declare several variables at once and assign their values later when using let. Keep in mind that we cannot do this with const.
     let authorName, category, optionTag;
     if (!$(this).hasClass('template')) {
@@ -46,9 +46,11 @@ articleView.handleAuthorFilter = function() {
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
       $('article').hide();
       $('article[data-author ="' + authorName + '"]').show();
-      //$('this.val').fadeIn('slow')
+      $('this.val').fadeIn('slow');
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+      $('article').fadeIn('slow'); //taking article object from articles array and showing them.
+      $('article').template.hide(); //template used to clone new articles is being hidden.
     }
     $('#category-filter').val('');
   });
@@ -59,6 +61,7 @@ articleView.handleCategoryFilter = function() {
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
+  
 
 };
 
@@ -74,7 +77,8 @@ articleView.handleMainNav = function() {
 articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
-
+  // e.preventDefault();
+  // if ($(this.)text())
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
